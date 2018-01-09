a1589bd492db6eb55eef9c096a4810ecf9c5ae80
Nigel Yu <yunigel@ca.ibm.com>
2018-01-09 16:47:47 -0500

    update for zos

--- c++/Portal.h
+++ c++/Portal.h
@@ -29,45 +29,44 @@
 namespace consbench {
 
 // Native class template
-template<class PTR, class DERIVED> class FooJniClass {
- public:
-  // Get the java class id
-  static jclass getJClass(JNIEnv* env, const char* jclazz_name) {
-    jclass jclazz = env->FindClass(jclazz_name);
-    assert(jclazz != nullptr);
-    return jclazz;
-  }
+template<class PTR, class DERIVED>
+class FooJniClass {
+public:
+	// Get the java class id
+	static jclass getJClass(JNIEnv* env, const char* jclazz_name) {
+		jclass jclazz = env->FindClass(jclazz_name);
+		assert(jclazz != nullptr);
+		return jclazz;
+	}
 
-  // Get the field id of the member variable to store
-  // the ptr
-  static jfieldID getHandleFieldID(JNIEnv* env) {
-    static jfieldID fid = env->GetFieldID(
-        DERIVED::getJClass(env), "_nativeHandle", "J");
-    assert(fid != nullptr);
-    return fid;
-  }
+	// Get the field id of the member variable to store
+	// the ptr
+	static jfieldID getHandleFieldID(JNIEnv* env) {
+		static jfieldID fid = env->GetFieldID(DERIVED::getJClass(env),
+				"_nativeHandle", "J");
+		assert(fid != nullptr);
+		return fid;
+	}
 
-  // Get the pointer from Java
-  static PTR getHandle(JNIEnv* env, jobject jobj) {
-    return reinterpret_cast<PTR>(
-        env->GetLongField(jobj, getHandleFieldID(env)));
-  }
+	// Get the pointer from Java
+	static PTR getHandle(JNIEnv* env, jobject jobj) {
+		return reinterpret_cast<PTR>(env->GetLongField(jobj,
+				getHandleFieldID(env)));
+	}
 
-  // Pass the pointer to the java side.
-  static void setHandle(JNIEnv* env, jobject jdb, PTR ptr) {
-    env->SetLongField(
-        jdb, getHandleFieldID(env),
-        reinterpret_cast<jlong>(ptr));
-  }
+	// Pass the pointer to the java side.
+	static void setHandle(JNIEnv* env, jobject jdb, PTR ptr) {
+		env->SetLongField(jdb, getHandleFieldID(env),
+				reinterpret_cast<jlong>(ptr));
+	}
 };
 
-
 // The portal class for com.jni.consbench.FooByCallInvoke
-class FooByCallInvokeJni : public FooJniClass<consbench::Foo*, FooByCallInvokeJni> {
- public:
-  static jclass getJClass(JNIEnv* env) {
-    return FooJniClass::getJClass(env, "com/jni/consbench/FooByCallInvoke");
-  }
+class FooByCallInvokeJni: public FooJniClass<consbench::Foo*, FooByCallInvokeJni> {
+public:
+	static jclass getJClass(JNIEnv* env) {
+		return FooJniClass<consbench::Foo*, FooByCallInvokeJni>::getJClass(env, "com/jni/consbench/FooByCallInvoke");
+	}
 };
 
 } //end namespace
