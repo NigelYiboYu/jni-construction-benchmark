7ddbe1ca2e0735c677c8f29c3c5b98ef735b1b72
Nigel Yu <yunigel@ca.ibm.com>
2018-01-15 14:38:10 -0500

    update

--- c++/javabacked/FooByCall.cpp
+++ c++/javabacked/FooByCall.cpp
@@ -12,7 +12,7 @@ jlong Java_com_jni_consbench_javabacked_FooByCall_newFoo(JNIEnv* env,
 	// create a java obj via JNI and retrun the _javaObjHandle in Java
 	jclass newCls = env->FindClass("com/jni/consbench/javabacked/FooJavaObject");
 	jmethodID initMethod = env->GetMethodID(newCls, "<init>", "()V");
-	jobject newObj = env->NewObject(env, newCls, initMethod);
+	jobject newObj = env->NewObject(newCls, initMethod);
 
 	return reinterpret_cast<jlong>(newObj);
 }
