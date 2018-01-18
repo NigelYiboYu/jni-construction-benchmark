7d62594b0744a3d246bc64e3e663b5c23fc7e7cf
Nigel Yu <yunigel@ca.ibm.com>
2018-01-18 11:10:47 -0500

    update

--- c++/make-all-cpp-zos.sh
+++ c++/make-all-cpp-zos.sh
@@ -4,32 +4,20 @@ rm -f ./*.so
 export INC_PATH=" -I/jit/team/yunigel/jni-bench/src  -I/jit/team/yunigel/sdk/include/zos -I/jit/team/yunigel/sdk/include/ "
 
 
-# make a Std non-xplink DLL
+CPP_FILES=`find . | grep '\.cpp' | tr '\n' ' '`
+
+# make a Std non-xplink JNI benchmark DLL
 #
 #
 #		NOTE: xlc can't handle files with the same name. Hence, the **JavaObject.cpp file names.
 #	
-echo  "Making std non-xplink DLL"
-export CXXFLAGS=" -Dnullptr=NULL -Wc,convlit(ISO8859-1) -Wc,NOANSIALIAS -Wc,noxplink -O -qlanglvl=extended0x -Wc,DLL,EXPORTALL -Wa,DLL "
+echo  "Making std non-xplink JNI benchmark  DLL"
+export CXXFLAGS="-Dnullptr=NULL -Wc,convlit(ISO8859-1) -Wc,NOANSIALIAS -Wc,noxplink -O -qlanglvl=extended0x -Wc,DLL,EXPORTALL -Wa,DLL "
+xlC $CXXFLAGS -o libstdlinkjnibench.so $INC_PATH $CPP_FILES
 
-xlC $CXXFLAGS -o libstdlinkjnibench.so $INC_PATH \
-	./nativeobj/Foo.cpp ./nativeobj/FooByCall.cpp \
-	./nativeobj/FooByCallStatic.cpp ./nativeobj/FooByCallInvoke.cpp \
-	./javaobj/FooByCallJavaObject.cpp ./javaobj/FooByCallStaticJavaObject.cpp ./javaobj/FooByCallInvokeJavaObject.cpp \
-	./simplecall/SimpleCall.cpp  
-	
-	
-	
-	
-echo  "Making 64-bit xplink DLL"
+
+
+echo  "Making 64-bit xplink JNI benchmark DLL"
 rm -f ./*.o
 export CXXFLAGS=" -Dnullptr=NULL -Wc,convlit(ISO8859-1) -Wc,NOANSIALIAS -Wc,xplink -O -qlanglvl=extended0x -Wc,lp64 -Wc,DLL,EXPORTALL -Wa,DLL "
-xlC $CXXFLAGS -o libxplinkjnibench.so $INC_PATH \
-	./nativeobj/Foo.cpp ./nativeobj/FooByCall.cpp \
-	./nativeobj/FooByCallStatic.cpp ./nativeobj/FooByCallInvoke.cpp \
-	./javaobj/FooByCallJavaObject.cpp ./javaobj/FooByCallStaticJavaObject.cpp ./javaobj/FooByCallInvokeJavaObject.cpp \
-	./simplecall/SimpleCall.cpp  
-
-	
-	
-	
+xlC $CXXFLAGS -o libxplinkjnibench.so $INC_PATH $CPP_FILES
--- c++/make-all-cpp.sh
+++ c++/make-all-cpp.sh
@@ -4,14 +4,11 @@ rm -f ./*.so
 export BENCH_HOME="/jit/team/yunigel/jni-bench"
 export OPENJDK_HOME="/jit/team/yunigel/openJDK9/jdk-9+181"
 
+CPP_FILES=`find . | grep '\.cpp' | tr '\n' ' '`
 
 CXXFLAGS="-shared -fPIC -std=c++11 -O1"
 INC_PATH="-I$BENCH_HOME/src -I$OPENJDK_HOME/include/ -I$OPENJDK_HOME/include/linux "
 
 
 # use g++. gcc requires -lstdc++
-g++ $CXXFLAGS -o libjnibench.so $INC_PATH \
-	./nativeobj/Foo.cpp ./nativeobj/FooByCall.cpp \
-	./nativeobj/FooByCallStatic.cpp ./nativeobj/FooByCallInvoke.cpp \
-	./javaobj/FooByCallJavaObject.cpp ./javaobj/FooByCallStaticJavaObject.cpp ./javaobj/FooByCallInvokeJavaObject.cpp \
-	./simplecall/SimpleCall.cpp  
+g++ $CXXFLAGS -o libjnibench.so $INC_PATH $CPP_FILES
